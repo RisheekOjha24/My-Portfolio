d940d86afa603b76754a27468116621484ec50df
Risheek Ojha <risheekojha@gmail.com>
2024-08-09 10:31:37 +0530

    added lazy loading in myImage

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+// src/components/Spinner.jsx
+import React from "react";
+
+
+function Spinner() {
+  return <div className={"spinner"}>{/* Spinner styling here */}</div>;
+}
+
+export default Spinner;
--- src/pages/Btn1Page.jsx
+++ src/pages/Btn1Page.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import styles from "../styles/Btn1Page.module.css";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css"; // Optional: for blur effect
 import myImage from "../assets/myImage.jpg";
+import Spinner from "../components/Spinner"; // Import a Spinner component
 
 function Btn1Page() {
   return (
@@ -23,8 +26,14 @@ function Btn1Page() {
       </div>
 
       <div className={styles.RightPart}>
-        <div alt="big circle" className={styles.BigCircle}>
-          <img src={myImage} alt="my image" className={styles.MyImage} />
+        <div className={styles.BigCircle}>
+          <LazyLoadImage
+            src={myImage}
+            alt="my image"
+            className={styles.MyImage}
+            effect="blur" // Optional: adds a blur effect while loading
+            placeholder={<Spinner />} // Shows spinner while image is loading
+          />
         </div>
       </div>
     </div>
